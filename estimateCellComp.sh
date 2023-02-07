eaa163d4b214abf8473b06cdb5c03fbdd9a7dade
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2023-02-07 15:28:31 -0600

    Create estimateCellComp.sh

---
+++ estimateCellComp.sh
@@ -0,0 +1,15 @@
+# Estimate cell fractions using DXM (arf arf!)
+# deprecated...not used due to small sample size
+
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 501_Blood.bed -o estimatedFracs.501.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 502_Blood.bed -o estimatedFracs.502.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 503_Blood.bed -o estimatedFracs.503.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 512_Blood.bed -o estimatedFracs.512.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 514_Blood.bed -o estimatedFracs.514.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 515_Blood.bed -o estimatedFracs.515.Blood.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 501_HC.bed -o estimatedFracs.501.HC.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 502_HC.bed -o estimatedFracs.502.HC.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 503_HC.bed -o estimatedFracs.503.HC.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 512_HC.bed -o estimatedFracs.512.HC.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 514_HC.bed -o estimatedFracs.514.HC.txt
+python /media/Data/WGBS/DXM/dxm/dxm_estimateFracs.py -k 6 -i 515_HC.bed -o estimatedFracs.515.HC.txt
